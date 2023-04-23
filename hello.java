87026bfb06f59d07e90804eb5c13e9fdfae9d418
Siva7787 <96621028+Siva7787@users.noreply.github.com>
2023-04-23 22:51:37 +0530

    this is new commit

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 class hello{
   public static void main (String [] args)
   {
-    System.out.println("hello");
+      System.out.println("hello");
+	System.out.println("hii");
+	System.out.println("shiva");
   }
 }
   
