9c1f1ddab9e231242f865665d7411b57eb9b7166
Siva7787 <96621028+Siva7787@users.noreply.github.com>
2023-04-24 10:23:12 +0530

    new one

--- hello.java
+++ hello.java
@@ -1,9 +1,6 @@
-class hello{
-  public static void main (String [] args)
-  {
-      System.out.println("hello");
-	System.out.println("hii");
-	System.out.println("shiva");
+  class hello{
+    public static void main(String [] args){
+
+	    System.out.println("hello java")
   }
-}
-  
+  }
